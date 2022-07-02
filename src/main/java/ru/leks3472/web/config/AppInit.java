22b60f508f5752906676babe9e0ce711fc73530b
Leks3472 <ras.ufa@mail.ru>
2022-07-02 13:32:25 +0500

    Initial commit

---
+++ src/main/java/ru/leks3472/web/config/AppInit.java
@@ -0,0 +1,20 @@
+package ru.leks3472.web.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
